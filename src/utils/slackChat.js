5f73cb578da6ceb13175f26b8ecfe4d54e371557
sujikim <sujikim@student.42seoul.kr>
2023-11-07 17:08:11 +0900

    feat: 슬랙 메세지 전송시 로그 기록 남기기

--- src/utils/slackChat.js
+++ src/utils/slackChat.js
@@ -4,17 +4,23 @@ const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
 
 export const sendText = async (to, text) => {
-  await web.chat.postMessage({
-    token,
-    channel: to,
-    text,
-  });
+  await web.chat
+    .postMessage({
+      token,
+      channel: to,
+      text,
+    })
+    .then(() => console.log(to, text, '메세지를 보냈습니다. '));
 };
 
 export const sendBlocks = async (to, blocks) => {
-  await web.chat.postMessage({
-    token,
-    channel: to,
-    blocks,
-  });
+  await web.chat
+    .postMessage({
+      token,
+      channel: to,
+      blocks,
+    })
+    .then(() =>
+      console.log(to, JSON.stringify(blocks), ' 메세지를 보냈습니다. ')
+    );
 };
