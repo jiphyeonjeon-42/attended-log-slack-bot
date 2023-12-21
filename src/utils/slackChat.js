53341c33c0cb5baf9e86bbb5e296ac653e470d68
sujikim <sujikim@student.42seoul.kr>
2023-12-22 08:39:26 +0900

    fix: 타임아웃으로 인한 개별 로깅 삭제

--- src/utils/slackChat.js
+++ src/utils/slackChat.js
@@ -4,23 +4,17 @@ const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
 
 export const sendText = async (to, text) => {
-  await web.chat
-    .postMessage({
-      token,
-      channel: to,
-      text,
-    })
-    .then(() => console.log(to, text, '메세지를 보냈습니다. '));
+  await web.chat.postMessage({
+    token,
+    channel: to,
+    text,
+  });
 };
 
 export const sendBlocks = async (to, blocks) => {
-  await web.chat
-    .postMessage({
-      token,
-      channel: to,
-      blocks,
-    })
-    .then(() =>
-      console.log(to, JSON.stringify(blocks), ' 메세지를 보냈습니다. ')
-    );
+  await web.chat.postMessage({
+    token,
+    channel: to,
+    blocks,
+  });
 };
