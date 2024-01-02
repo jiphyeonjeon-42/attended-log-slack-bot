917874f9c1dd0b4644103e7fabdeae342abf1c2e
sujikim <sujikim@student.42seoul.kr>
2024-01-02 09:41:18 +0900

    fix: 예약발송 문구 수정
    실패시 노출되는 문구가 아니라 앱 알림등에 노출되는 문구

--- src/constants/events.js
+++ src/constants/events.js
@@ -1,7 +1,6 @@
 export const ATTENDED = 'attended';
 export const MEETING = '정기사서회의';
 export const MEETING_MESSAGE_DELAY = 5;
-export const MEETING_FALLBACK_MESSAGE =
-  '이 메세지를 보신다면 @sujikim DM 부탁드려요';
+export const MEETING_FALLBACK_MESSAGE = '사서회의 참석여부 메세지입니다';
 export const SHIFT = '사서근무';
 export const SHIFT_WEEKEND = '주말사서근무';
